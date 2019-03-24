17ced8d440cd65070ac1905812968f963d3c866d
Victor Grycuk <victor@grycuk.net>
2019-03-23 22:47:43 -0300

    Refactor EvenNumbers

--- Diagnostico2019/Exercises/EvenNumbers.cs
+++ Diagnostico2019/Exercises/EvenNumbers.cs
@@ -8,21 +8,54 @@ namespace Diagnostico2019.Exercises
 {
     public static class EvenNumbers
     {
+        /// <summary>
+        /// Trtied to avoid the use of List<int> since the exercise used the word "vector"
+        /// </summary>
+
         public static void GetNumbers()
         {
-            int[] array = new int[] { 45, 23, 52, 68, 2, 98, 106, 107, 465, 23, 18 };
-            List<int> evenNumber = new List<int>();
+            int[] array = new int[] { };
 
-            foreach (var number in array)
+            Console.WriteLine("Add numbers to sort them in descending order. When done insert 'done'.");
+
+            int enteredNumber;
+            while (true)
             {
-                if (number % 2 == 0)
+                string line = Console.ReadLine();
+                if (int.TryParse(line, out enteredNumber))
                 {
-                    Console.WriteLine($"Number {number} is even" + Environment.NewLine);
-                    evenNumber.Add(number);
+                    array = array.Concat(new int[] { enteredNumber }).ToArray();
+                }
+                else if (line == "done")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Only numbers are accepted.");
                 }
             }
 
-            Console.WriteLine($"There are {evenNumber.Count} even numbers");
+            Console.WriteLine("====================");
+            foreach (var number in FilterEvenNumbers(array))
+            {
+                Console.WriteLine($"Number {number} is even");
+            }
+
+            Console.WriteLine(Environment.NewLine + $"There are {CountEvenNumbers(array)} even numbers");
+        }
+
+        /// The following two methods are not extrictly neccesary,
+        /// their logic was extracted just to be used for unit testing.
+
+        public static int[] FilterEvenNumbers(int[] number)
+        {
+            return number.Where(x => x % 2 == 0).ToArray();
+        }
+
+        public static int CountEvenNumbers(int[] number)
+        {
+            return number.Count(x => x % 2 == 0);
         }
     }
 }
