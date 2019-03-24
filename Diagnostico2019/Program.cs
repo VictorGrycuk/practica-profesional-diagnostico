2316bd57b42520f040f5b3054c1e4693f0c248e0
Victor Grycuk <victor@grycuk.net>
2019-03-23 21:20:22 -0300

    Add call to the menu

--- Diagnostico2019/Program.cs
+++ Diagnostico2019/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Diagnostico2019.Exercises;
 
 namespace Diagnostico2019
 {
@@ -10,12 +11,15 @@ namespace Diagnostico2019
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Menu menu = new Menu("== Diagnostico 2019 ==", "Please select one of the following items:");
+            menu.AddOption(EvenNumbers.GetNumbers, "Filter and show even numbers.");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            while (!menu.Exit)
+            {
+                Console.Clear();
+                menu.ShowMenu();
+                menu.GetSelection();
+            }
         }
     }
 }
