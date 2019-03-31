7da02daa977c6dfedb2410ef1ec70ae38736dd0f
Victor Grycuk <victor@grycuk.net>
2019-03-31 02:51:00 -0300

    Add entropy, Add padding

--- Diagnostico2019/Exercises/Hash.cs
+++ Diagnostico2019/Exercises/Hash.cs
@@ -11,25 +11,73 @@ namespace Diagnostico2019.Exercises
     {
         public static void Encode()
         {
-            int[] result = new int[16];
-            byte[] ba = Encoding.Default.GetBytes("sample");
-            var hexString = BitConverter.ToString(ba).Replace("-", string.Empty);
+            var result = Encode("Fadasdasd");
+        }
 
-            for (int i = 0; i < hexString.Length; i++)
+        public static string Encode(string value)
+        {
+            // Get the first byte as binary
+            byte[] bytesArray = Encoding.Default.GetBytes(value);
+            string binaryString = Convert.ToString(bytesArray[0], 2).PadLeft(8, '0');
+            int firstQuartet = Convert.ToInt32(binaryString.Substring(0, 4), 2);
+            int secondQuartet = Convert.ToInt32(binaryString.Substring(4), 2);
+
+            int repetitions = Fibonacci.GetSequence(firstQuartet + secondQuartet).Last();
+
+            string result = value;
+            for (int i = 0; i < repetitions; i++)
             {
-                if (i == 0)
-                {
-                    int index = GetHexValue(hexString[hexString.Length - 1]);
-                    var hexResult = AddTwoHexa(result[index], GetHexValue(hexString[i]));
-                    result[index] = hexResult.result;
-                }
-                else
-                {
-                    int index = GetHexValue(hexString[i - 1]);
-                    Update(result, index, hexString[i]);
-                }
+                result = Calculate(result);
+                result = result.Length != 16 ? GeneratePadding(result) : result;
             }
 
+            return result;
+        }
+
+        private static string Calculate(string value)
+        {
+            int[] result = new int[16];
+            byte[] ba = Encoding.Default.GetBytes(value);
+            var hexString = BitConverter.ToString(ba).Replace("-", string.Empty);
+
+            int index = GetHexValue(hexString[hexString.Length - 1]);
+            Update(result, index, hexString[0]);
+
+            for (int i = 1; i < hexString.Length; i++)
+            {
+                index = GetHexValue(hexString[i - 1]);
+                Update(result, index, hexString[i]);
+            }
+
+            var finalResult = string.Concat(result.Select(x => x.ToString("X")).ToArray());
+            finalResult = InvertString(finalResult);
+
+            return finalResult;
+        }
+
+        private static string GeneratePadding(string value)
+        {
+            if (value.Length == 16)
+            {
+                return value;
+            }
+
+            var neededPadding = 16 - value.Length;
+            var padding = string.Empty;
+
+            while (padding.Length <= neededPadding)
+            {
+                padding += InvertString(Calculate(value));
+            }
+
+            return padding.Substring(0, neededPadding) + value;
+        }
+
+        private static string InvertString(string value)
+        {
+            char[] charArray = value.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray).TrimStart(new char[] { '0' } );
         }
 
         private static bool Update(int[] array, int index, char value)
