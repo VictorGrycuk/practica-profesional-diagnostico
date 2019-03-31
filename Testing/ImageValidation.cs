fb632f62e32245f34f16e3e7c83ce52a731e8d24
Victor Grycuk <victor@grycuk.net>
2019-03-30 21:48:24 -0300

    Add unit tests

---
+++ Testing/ImageValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Diagnostico2019.Exercises;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestImageValidation
+    {
+        [TestMethod]
+        public void ValidateSize()
+        {
+            Assert.IsTrue(ImageValidation.CheckImagesSize(GetTestImage("Image1.png"), GetTestImage("Image4.png")));
+
+            Assert.IsTrue(!ImageValidation.CheckImagesSize(GetTestImage("Image1.png"), GetTestImage("Image2.png")));
+        }
+
+        [TestMethod]
+        public void ValidateDimensions()
+        {
+            Assert.IsTrue(ImageValidation.CheckImagesDimensions(GetTestImage("Image1.png"), GetTestImage("Image2.png")));
+
+            Assert.IsTrue(!ImageValidation.CheckImagesDimensions(GetTestImage("Image1.png"), GetTestImage("Image3.png")));
+        }
+
+        [TestMethod]
+        public void ValidateStream()
+        {
+            Assert.IsTrue(ImageValidation.CheckImagesStream(GetTestImage("Image1.png"), GetTestImage("Image4.png")));
+
+            Assert.IsTrue(!ImageValidation.CheckImagesStream(GetTestImage("Image1.png"), GetTestImage("Image3.png")));
+        }
+
+        public string GetTestImage(string name)
+        {
+            return Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "TestFiles" , name);
+        }
+    }
+}
