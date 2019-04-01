6f3ff3a296cbec4672809aa0674531c5062fd334
Victor Grycuk <victor@grycuk.net>
2019-03-31 22:52:06 -0300

    Add Unit Test

---
+++ Testing/TwoPoints.cs
@@ -0,0 +1,17 @@
+﻿using Diagnostico2019.Exercises;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestTwoPoints
+    {
+        [TestMethod]
+        public void CalculateDistance()
+        {
+            var pointA = new double[] { 2, 5 };
+            var pointB = new double[] { 7, -2 };
+            Assert.IsTrue(TwoPoints.CalculateDistance(pointA, pointB) == 8.6023252670426267);
+        }
+    }
+}
