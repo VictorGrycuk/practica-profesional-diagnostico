51a1fc126a96385ece10233c9be20e99f44b7dc6
VictorGrycuk <victor@grycuk.net>
2019-04-01 16:37:27 -0300

    Add unit tests

---
+++ Testing/ParabolicProjectile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Diagnostico2019.Exercises;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestParabolicProjectile
+    {
+        readonly int velocity = 18;
+        readonly int angle = 40;
+
+        [TestMethod]
+        public void CalculateDistance()
+        {
+            Assert.IsTrue((int)ParabolicProjectile.CalculateMaximumDistance(velocity, angle) == 32);
+        }
+
+        [TestMethod]
+        public void CalculateHeight()
+        {
+            Assert.IsTrue((int)ParabolicProjectile.CalculateMaximumHeight(velocity, angle) == 6);
+        }
+    }
+}
